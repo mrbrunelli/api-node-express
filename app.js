b674c354a9b05946a176c8ca8d202cb1d062cb25
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-04-28 17:59:20 -0300

    Post realizado com sucesso!.

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('./models/Artigo')
+const Artigo = mongoose.model('artigo')
 
 const app = express()
 
 app.use(express.json())
 
+mongoose.connect('mongodb://localhost/teste', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conexão com o MongoDB realizada com sucesso!')
+}).catch((erro) => {
+    console.log('Erro ao conectar com o MongoDB!')
+})
+
 app.get('/', (req, res) => {
     return res.json({ titulo: 'Como criar a API' })
+})
+
+app.post('/artigo', (req, res) => {
+    const artigo = Artigo.create(req.body, (err) => {
+        if (err) return res.status(400).json({
+            error: true,
+            message: "Error: Artigo não foi cadastrado com sucesso!"
+        })
+
+        return res.status(200).json({
+            error: false,
+            message: "Artigo cadastrado com sucesso!"
+        })
+    })
 })
 
 app.listen(8080, () => {
